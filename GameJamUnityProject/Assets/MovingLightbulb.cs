4876be4b2979103a27a046ae5d1b848876ec01d4
Hubchris <Gwanplants@Christophers-iMac.local>
2017-04-22 11:29:49 +0200

    clicking functionality - tags and prefab creation

---
+++ GameJamUnityProject/Assets/MovingLightbulb.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingLightbulb : MonoBehaviour {
+
+	float speed = 10000f;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, Input.mousePosition, step);
+	}
+}
---
+++ GameJamUnityProject/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInput : MonoBehaviour {
+
+	//Input bools
+	bool mouseClick;
+	bool mousHold;
+	bool mouseRelease;
+
+	//Lightbulb
+	public Image lightBulb;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		mouseClick = Input.GetMouseButtonDown(0);
+		mousHold = Input.GetMouseButton(0);
+		mouseRelease = Input.GetMouseButtonUp(0);
+
+		if (mouseClick)
+		{
+			Debug.Log("clicked mouse");
+
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+			if (hit)
+			{
+				ItemClicked(hit.transform.gameObject.tag);
+			}
+			else 
+			{
+				Debug.Log("No hit");
+			}
+		} 
+		if (mousHold)
+		{
+			Debug.Log("held mouse");
+		}
+
+		if (mouseRelease)
+		{
+			Release();
+		}
+	}
+
+
+	public void ItemClicked (string tag)
+	{
+		if (tag == "newBulb")
+		{
+			Debug.Log("new lightbulb acquired");
+
+			if (mousHold)
+			{
+				lightBulb.enabled = true;
+			}
+			else if (mouseRelease)
+			{
+				lightBulb.enabled = false;
+			}
+		}
+		else if (tag == "rope")
+		{
+			Debug.Log("Zero Grav engaged");
+		}
+		else if (tag == "pump")
+		{
+			Debug.Log("Dooont you know pump it up!");
+		}
+		else if (tag == "flag")
+		{
+			Debug.Log("AMMMEEEEERICA! FUCK YEAH!");
+		}
+	}
+
+	public void Release()
+	{
+		if ( lightBulb.enabled == true)
+		{
+			lightBulb.enabled = false;
+		}
+	}
+}
