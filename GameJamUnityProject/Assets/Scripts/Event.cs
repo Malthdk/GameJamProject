8a9f400b3bccfc1f9ffbfbdf546a0cef4595ba14
Malte Hølledig <malte@holledig.dk>
2017-04-22 10:06:57 +0200

    scripts

---
+++ GameJamUnityProject/Assets/Scripts/Event.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour {
+
+	bool active;
+	bool damageable;
+	int difficulty;
+	float allowedReactionTime;
+	float damageIncreaseRate;
+	float damageMin, damageMax;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
