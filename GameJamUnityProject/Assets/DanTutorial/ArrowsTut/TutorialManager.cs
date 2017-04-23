1a703a101c11358641c8a9af2caa011f978f93c1
Hubchris <Gwanplants@Christophers-iMac.local>
2017-04-23 09:47:46 +0200

    lol

--- GameJamUnityProject/Assets/DanTutorial/ArrowsTut/TutorialManager.cs
+++ GameJamUnityProject/Assets/DanTutorial/ArrowsTut/TutorialManager.cs
@@ -32,7 +32,7 @@ public class TutorialManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.Space))
+		if (Input.GetKey(KeyCode.Backspace))
         {
             SceneManager.LoadScene(2);
         }
